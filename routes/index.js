45731dfe814be9ed31cbc71310ee4daea23efcd0
Ignacio <ignaciomdsu@gmail.com>
2019-05-11 15:31:00 +0900

    units per hour working

--- routes/index.js
+++ routes/index.js
@@ -52,146 +52,149 @@ fs.watchFile('data.csv', function (curr, prev){
 	});
 */
 
-/*
+
 fs.watchFile('data.csv', function (curr, prev){
-		csv()
-		.fromFile('data.csv')
+	csv()
+		.fromFile(require('path').join(__dirname, '..', 'data.csv'))
 		.then((jsonObj)=>{
 			results = jsonObj
-		    //console.log(jsonObj.State);
-    		var dates = results.map((o)=>moment(o.Timestamp).format("MM-DD-YYYY HH:mm:ss"))
-    		var states = results.map((o)=>o.State)
-    		values.state = states[states.length-1]
-    		console.log(dates)
-    		console.log(states)
-		    results.forEach(function(entry){
-		    	//console.log(entry.State)
-		    	//values.state = entry.State;
-		    	//console.log(entry.Timestamp)
-		    	if(entry.State == 'transit'){
-		    		values.current_pu+=1;
-		    	}
-		    })
-			})
+			var states = results.map((o)=>o.State)
+			//console.log(states)
 
-	});
-*/
+			
+			var val = results.reduce((acc, cur) => {
+
+
+				var today = new Date();
+				
+				var today_day = today.getDate();
+				var today_month =  today.getMonth() + 1;
+				var today_year =  today.getFullYear();
+
+				var date  = new Date(cur.Timestamp);
+				var hour = date.getHours();
+				var minutes = date.getMinutes();
+				var seconds = date.getTime()/1000+9*3600*1000;
+				var day =date.getDate();
+				var month = date.getMonth()+1;
+				var year = date.getFullYear();
+				//console.log(hour)
+				//console.log(cur.State)
+				//if(today_day == day & today_month == month & today_year == year){
+				//Calculates the last state	
+				values.state = states[states.length-1]
+
+				//Calculates how many units and avg
+				if(acc.lastTime == 0) acc.lastTime = seconds 
+				if(acc.precedent_hour == 0) acc.precedent_hour = hour
+				//console.log(cur.State)
+				if(hour == acc.precedent_hour){
+					if(acc.lastState !== cur.State && cur.State == 'transit'){
+
+						acc.count += 1
+
+						acc.avg += seconds - acc.lastTime;
+											
+						acc.it += 1;
+						acc.lastTime = seconds
+
+					
+						acc.count_per_hour[acc.iteration_vector]+=1;
+						acc.precedent_hour = hour;
+					}
+				}else{
+					acc.iteration_vector+=1;
+					acc.precedent_hour = hour;
+				}
+				
+				acc.lastState = cur.State
+
+
+				//How many units per hour
+				//console.log(acc.precedent_hour)
+
+
+
+				//}
+				return acc
+			}, {lastState: undefined, count: 0, lastTime: 0, avg: 0, it: 0, precedent_hour:0, iteration_vector:0, count_per_hour:[0,0,0,0,0,0,0,0]})
+			values.units_left = dairly_units - val.count
+			values.pitch_avg =  (val.avg/val.it).toFixed(2)
+			values.units_perhour = val.count_per_hour
+			console.log(values.units_perhour)
+		})
+});
 
 
 router.get('/', function (req, res, next) {
 		
-	  csv()
+	csv()
 		.fromFile(require('path').join(__dirname, '..', 'data.csv'))
 		.then((jsonObj)=>{
 			results = jsonObj
-		    //console.log(jsonObj.State);
-    		var dates = results.map((o)=>moment(o.Timestamp).format("MM-DD-YYYY HH:mm:ss"))
-    		var states = results.map((o)=>o.State)
-    		values.state = states[states.length-1]
-			values.units_left = dairly_units - results.reduce((acc, cur) => {
+			var states = results.map((o)=>o.State)
+			
 
-				if(acc.lastState !== cur.State && cur.State == 'transit') acc.count += 1
-				acc.lastState = cur.State
+			
+			var val = results.reduce((acc, cur) => {
+
+
+				var today = new Date();
+				
+				var today_day = today.getDate();
+				var today_month =  today.getMonth() + 1;
+				var today_year =  today.getFullYear();
+
+				var date  = new Date(cur.Timestamp);
+				var hour = date.getHours();
+				var minutes = date.getMinutes();
+				var seconds = date.getTime()/1000+9*3600*1000;
+				var day =date.getDate();
+				var month = date.getMonth()+1;
+				var year = date.getFullYear();
+
+				//console.log(hour)
+				//if(today_day == day & today_month == month & today_year == year){
+				//Calculates the last state	
+				values.state = states[states.length-1]
+
+				//Calculates how many units and avg
+				if(acc.lastTime == 0) acc.lastTime = seconds 
+				if(acc.precedent_hour == 0) acc.precedent_hour = hour
+				//console.log(cur.State)
+				if(hour == acc.precedent_hour){
+					if(acc.lastState !== cur.State && cur.State == 'transit'){
+
+						acc.count += 1
+						
+						acc.avg += seconds - acc.lastTime;
+											
+						acc.it += 1;
+						acc.lastTime = seconds
+
+						acc.count_per_hour[acc.iteration_vector]+=1;
+
+						acc.precedent_hour = hour;
+
+					}else{
+						acc.iteration_vector+=1;
+						acc.precedent_hour = hour;
+					}
+
+					acc.lastState = cur.State
+				}
 				return acc
-			}, {lastState: undefined, count: 0}).count
-
-			})
-		
-      res.render('base',{ 
-      	values: values,
-      	//config: config
-      });
-
-      //values.current_pu = 0
-    })
+			}, {lastState: undefined, count: 0, lastTime: 0, avg: 0, it: 0, precedent_hour:0, iteration_vector:0, count_per_hour:[0,0,0,0,0,0,0,0]})
+			values.units_left = dairly_units - val.count
+			values.pitch_avg =  (val.avg/val.it).toFixed(2)
+			values.units_perhour = val.count_per_hour
+			//console.log(val.count_per_hour)
+		})
+	res.render('base', values)
+})
 
 
 router.post('/getdata', (req, res) => {
-
-
-	csv()
-	.fromFile(require('path').join(__dirname, '..', 'data.csv'))
-	.then((jsonObj)=>{
-		results = jsonObj
-	    //console.log(jsonObj.State);
-		/*var dates = results.reduce((acc, cur)=>{
-
-			if(acc.lastState !== cur.State && cur.State == 'transit'){
-				var date  = new Date(cur.Timestamp);
-				var seconds = date.getTime();
-				acc.avg+=seconds-lastTime;
-				it+=1;
-
-				acc.lastState = cur.State	
-
-			}
-			acc.lastTime = seconds
-		},{lastState: undefined, lastTime:0, avg:0, it:0})
-		console.log(dates)	*/
-
-		var states = results.map((o)=>o.State)
-		
-
-		
-		var val = results.reduce((acc, cur) => {
-
-
-			var today = new Date();
-			
-			var today_day = today.getDate();
-			var today_month =  today.getMonth() + 1;
-			var today_year =  today.getFullYear();
-
-			var date  = new Date(cur.Timestamp);
-			var hour = date.getHours();
-			var minutes = date.getMinutes();
-			var seconds = date.getTime()/1000+9*3600*1000;
-			var day =date.getDate();
-			var month = date.getMonth()+1;
-			var year = date.getFullYear();
-
-			//console.log(hour)
-			if(today_day == day & today_month == month & today_year == year){
-			//Calculates the last state	
-			values.state = states[states.length-1]
-
-			//Calculates how many units and avg
-			if(acc.lastTime == 0) acc.lastTime = seconds 
-			if(acc.precedent_hour == 0) acc.precedent_hour = hour
-			console.log(cur.State)
-			if(acc.lastState !== cur.State && cur.State == 'transit'){
-
-				acc.count += 1
-				
-				acc.avg += seconds - acc.lastTime;
-									
-				acc.it += 1;
-				acc.lastTime = seconds
-
-			} 
-			acc.lastState = cur.State
-
-
-			//How many units per hour
-			//console.log(acc.precedent_hour)
-			if(hour == acc.precedent_hour){
-				acc.count_per_hour[acc.iteration_vector]+=1;
-
-				acc.precedent_hour = hour;
-			}else{
-				acc.iteration_vector+=1;
-				acc.precedent_hour = hour;
-			}
-
-
-			}
-			return acc
-		}, {lastState: undefined, count: 0, lastTime: 0, avg: 0, it: 0, precedent_hour:0, iteration_vector:0, count_per_hour:[0,0,0,0,0,0,0,0]})
-		values.units_left = dairly_units - val.count
-		values.pitch_avg =  (val.avg/val.it).toFixed(2)
-		values.units_perhour = val.count_per_hour
-		//console.log(val.count_per_hour)
-		})
 
 	res.send({values})
 })
